041167ed6e8af58c3cb04dd4d2c87d5b805fd4a3
Leticia <leticia.marson27@gmail.com>
2022-02-23 18:12:08 +0300

    ultimas atualizações

---
+++ lib/config.dart
@@ -0,0 +1,20 @@
+class Configurations {
+  static const _apiKey = "AIzaSyCa9txuNW0j4NPNuGEQ5OqkZzj75_OmJ9U";
+  static const _authDomain = "multiplayergame-9c158.firebaseapp.com";
+  static const _projectId = "multiplayergame-9c158";
+  static const _storageBucket = "multiplayergame-9c158.appspot.com";
+  static const _messagingSenderId = "1034048064092";
+  static const _appId = "1:1034048064092:web:2283cc0554fa8406083499";
+  static const _measurementId = "G-C10L9EC2CS";
+  static const _databaseId="https://multiplayergame-9c158-default-rtdb.firebaseio.com/";
+
+
+  String get apiKey => _apiKey;
+  String get authDomain => _authDomain;
+  String get projectId => _projectId;
+  String get storageBucket => _storageBucket;
+  String get messagingSenderId => _messagingSenderId;
+  String get appId => _appId;
+  String get measurementId=>_measurementId;
+  String get databaseId=>_databaseId;
+}
