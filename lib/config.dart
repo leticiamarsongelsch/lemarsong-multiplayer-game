fb2845aa297a6b8d080cb9c5a5d5ef1d01691d7c
Leticia <leticia.marson27@gmail.com>
2022-09-14 21:30:45 +0300

    ultimas atualizações

--- lib/config.dart
+++ lib/config.dart
@@ -4,8 +4,8 @@ class Configurations {
   static const _projectId = "multiplayergame-9c158";
   static const _storageBucket = "multiplayergame-9c158.appspot.com";
   static const _messagingSenderId = "1034048064092";
-  static const _appId = "1:1034048064092:web:2283cc0554fa8406083499";
-  static const _measurementId = "G-C10L9EC2CS";
+  static const _appId = "1:1034048064092:web:679f0c76badca904083499";
+  static const _measurementId = "G-FGCTLCHBWW";
   static const _databaseId="https://multiplayergame-9c158-default-rtdb.firebaseio.com/";
 
 
